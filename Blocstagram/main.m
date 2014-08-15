3db24a5f2d9ee79bfeb4b6659440ac0560f3dd7e
Kervins Valcourt <kervins.valcourt@gmail.com>
2014-08-15 03:21:50 -0400

    Initial Commit

---
+++ Blocstagram/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Blocstagram
+//
+//  Created by Kervins Valcourt on 8/15/14.
+//  Copyright (c) 2014 EastoftheWestEnd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "EWEAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([EWEAppDelegate class]));
+    }
+}
